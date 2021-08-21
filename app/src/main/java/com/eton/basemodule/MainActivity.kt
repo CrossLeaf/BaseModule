37906d44d8edbbe5fd97d9604f217e9f9d47d3a2
eton <eton@seektopser.com>
2021-08-21 23:59:21 +0800

    測試內部使用 library

--- app/src/main/java/com/eton/basemodule/MainActivity.kt
+++ app/src/main/java/com/eton/basemodule/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.eton.basemodule
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.eton.base_module.TestClass
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        TestClass().add(1, 2)
     }
 }
